40e22823fecabb0bc72a4e8b3dbc1deacd04a366
sean <sean.chen@gosaico.com>
2025-05-09 09:32:29 +0800

    awt034

--- src/openapi/registry.ts
+++
@@ -1,4 +0,0 @@
-import { OpenAPIRegistry } from '@asteasolutions/zod-to-openapi';
-
-// 創建並導出單例OpenAPI註冊表
-export const registry = new OpenAPIRegistry();
--- src/routes/user/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'hono';
-import { getUserOperator } from './getUser/operator.js';
-
-// 創建用戶路由
-export const userRoutes = new Hono();
-
-// 註冊獲取用戶路由
-userRoutes.get('/:id', getUserOperator);
